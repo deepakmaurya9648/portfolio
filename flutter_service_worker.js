13b40e720f9c0c523bd3e81f91027ad2fa148666
Deepak  Maurya <dkmaurya9648@gmail.com>
2021-05-03 08:41:10 +0530

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -18,8 +18,8 @@ const RESOURCES = {
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/NOTICES": "2ca73286ddca1b039673b75efe561b62",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"index.html": "0eac6e6d73499b151abf61fa2b231728",
-"/": "0eac6e6d73499b151abf61fa2b231728",
+"index.html": "60851be316268ec1647ae6f705a294d8",
+"/": "60851be316268ec1647ae6f705a294d8",
 "main.dart.js": "28633bd5777328e7f74c2c0f9f7830f9",
 "manifest.json": "3421cd00e17d6f742428a18cbf45709d",
 "version.json": "1cefaf2a48f42633ff4b37ffe0e9de96"
